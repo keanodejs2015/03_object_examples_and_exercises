1f4ce1d32d5ed525fac0efcba9e26eac5eeed1d8
Claus Bové <clbo@kea.dk>
2015-09-09 12:06:44 +0200

    01 added

---
+++ exercises/01_exercise_object_literal.js
@@ -0,0 +1,23 @@
+// create an object called person using an object literal
+// the object should have the properties:
+// name, age, gender
+
+_
+
+// print the whole object, the name, the age and the gender 
+// in 4 console.log statements
+
+_
+
+// change the persons name, age and gender
+
+_
+
+// Create a second object called animal using an Object literal
+// It should should have the 2 properties name and age.
+// name should be public accessible but age should be private, 
+// and should be accessible through a getter and a setter
+
+_
+
+
